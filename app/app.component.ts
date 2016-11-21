b1a37ad095d749761b0bcdf8b26f4592515c094a
rgalati <roberto.galati6@gmail.com>
2016-11-21 11:27:28 +0100

    First

---
+++ app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component }          from '@angular/core';
+
+@Component({
+    //moduleId: module.id,
+    selector: 'my-app',
+    template: `
+    <h1>{{title}}</h1>
+    
+    `
+})
+export class AppComponent {
+    title = 'Roboine-Shop';
+}
